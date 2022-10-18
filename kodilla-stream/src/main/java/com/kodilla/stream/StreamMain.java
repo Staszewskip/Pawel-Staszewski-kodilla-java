463b3595b33e848fec73c466c47b00ee6216674e
Pawel Staszewski <p.staszewski1@gmail.com>
2022-10-18 21:08:24 +0200

    7.1 upiekszacz tekstów

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifer;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("7.1 text beautifier");
+        PoemBeautifer poemBeautifer = new PoemBeautifer();
+        poemBeautifer.beautify("barcelona", text -> text.toUpperCase());
+        poemBeautifer.beautify("Barcelona", text -> "FC "+ text);
+        poemBeautifer.beautify("barcelona", text -> text.replace("b", "B"));
+        poemBeautifer.beautify("   Barcelona    ", text -> text.trim());
+    }
+}
