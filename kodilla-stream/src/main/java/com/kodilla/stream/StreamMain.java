c60756b07a392c8411927b3d731cb23481e25ba6
Pawel Staszewski <p.staszewski1@gmail.com>
2022-10-19 23:07:50 +0200

    7.4 liczba ludzi na swiecie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,34 +1,28 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifer;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
         System.out.println("7.1 text beautifier");
         PoemBeautifer poemBeautifer = new PoemBeautifer();
         poemBeautifer.beautify("barcelona", text -> text.toUpperCase());
-        poemBeautifer.beautify("Barcelona", text -> "FC "+ text);
+        poemBeautifer.beautify("Barcelona", text -> "FC " + text);
         poemBeautifer.beautify("barcelona", text -> text.replace("b", "B"));
         poemBeautifer.beautify("   Barcelona    ", text -> text.trim());
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+
+//        System.out.println("7.3 funkcyjny spacer po liście użytkowników forum");
+//        Forum forum = new Forum();
+//        Map<Integer, ForumUser> userMap = forum.getUserList().stream();
+//                .filter(forumUser -> forumUser.getSex() == 'M')
+//                .filter(forumUser -> LocalDate.now().getYear() - forumUser.getBirthDate().getYear() >= 20)
+//                .filter(forumUser -> forumUser.getNbOfPosts() >= 1)
+//                .collect(Collectors.toMap(ForumUser::getId, forumUser -> forumUser));
+//
+//               userMap.entrySet().stream()
+//                       .map(entry -> entry.getKey() + ": " + entry.getValue())
+//                       .forEach(System.out::println);
     }
 }
