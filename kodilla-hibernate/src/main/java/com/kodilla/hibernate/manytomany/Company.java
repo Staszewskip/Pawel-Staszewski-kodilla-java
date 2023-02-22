9a7265c9cab0ce54234a4bca6acac4c32db13fe4
Pawel Staszewski <p.staszewski1@gmail.com>
2023-02-22 11:11:11 +0100

    24.2

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -16,7 +16,6 @@ import java.util.List;
                 query = "FROM Company WHERE name LIKE concat('%',:ARG, '%')"
         )
 }
-
 )
 
 @Entity
---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -0,0 +1,69 @@
+package com.kodilla.patterns2.decorator.pizza;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzaOrderTestSuite {
+    @Test
+    public void testBasicPizza() {
+//        Given
+        PizzaOrder pizza = new BasicPizzaOrder();
+//        When
+        BigDecimal basicPizzaCost = pizza.getCost();
+        String basicPizzaDescription = pizza.getDescription();
+//        Then
+        assertAll(
+                () -> assertEquals(new BigDecimal(15), basicPizzaCost),
+                () -> assertEquals("Basic pizza ", basicPizzaDescription)
+        );
+    }
+    @Test
+    public void testVegetablesPizza() {
+//        Given
+        PizzaOrder pizza = new BasicPizzaOrder();
+        pizza = new VegatablesPizzaDecorator(pizza);
+//        When
+        BigDecimal cost = pizza.getCost();
+        String description = pizza.getDescription();
+//        Then
+        assertAll(
+                () -> assertEquals(new BigDecimal(18  ), cost),
+                () -> assertEquals("Basic pizza + pepper & onion ", description)
+        );
+    }
+
+    @Test
+    public void testVegetablesAndMeatPizza() {
+//        Given
+        PizzaOrder pizza = new BasicPizzaOrder();
+        pizza = new VegatablesPizzaDecorator(pizza);
+        pizza = new MeatPizzaDecorator(pizza);
+        //        When
+        BigDecimal cost = pizza.getCost();
+        String description = pizza.getDescription();
+//        Then
+        assertAll(
+                () -> assertEquals(new BigDecimal(23 ), cost),
+                () -> assertEquals("Basic pizza + pepper & onion + chorizo sausage ", description)
+        );
+    }
+    @Test
+    public void testFullOptionPizza() {
+//        Given
+        PizzaOrder pizza = new BasicPizzaOrder();
+        pizza = new VegatablesPizzaDecorator(pizza);
+        pizza = new MeatPizzaDecorator(pizza);
+        pizza = new ExtraCheesePizzaDecorator(pizza);
+        //        When
+        BigDecimal cost = pizza.getCost();
+        String description = pizza.getDescription();
+//        Then
+        assertAll(
+                () -> assertEquals(new BigDecimal(25 ), cost),
+                () -> assertEquals("Basic pizza + pepper & onion + chorizo sausage + extra cheese ", description)
+        );
+    }
+}
