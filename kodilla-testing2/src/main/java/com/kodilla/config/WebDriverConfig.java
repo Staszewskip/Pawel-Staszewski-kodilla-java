d534e4e12de167ad43163baeb654bc4faf517283
Paweł Staszewski <p.staszewski1@gmail.com>
2023-04-11 21:17:51 +0200

    31.1

--- kodilla-testing2/src/main/java/com/kodilla/config/WebDriverConfig.java
+++ kodilla-testing2/src/main/java/com/kodilla/config/WebDriverConfig.java
@@ -6,7 +6,6 @@ public class WebDriverConfig {
     public final static String CHROME = "CHROME_DRIVER";
     public static WebDriver getDriver(final String driver) {
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium drivers\\Chrome\\chromedriver.exe");
-
         if (driver.equals(CHROME)) {
             return new ChromeDriver();
         } else {
